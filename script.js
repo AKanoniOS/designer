f9f4836a1c2d6b4ff88bf320d148b24a3e35ff70
steve <schmeltzersteve@gmail.com>
2023-01-18 20:36:55 -0500

    transformer event listeners work like normal on existing shapes but not on new uploaded images

--- script.js
+++ script.js
@@ -68,36 +68,82 @@ group.add(image);
 layer.add(group);
 
 
-
+// TRANSFORMER
 // create a transformer
 var transformer = new Konva.Transformer();
 layer.add(transformer);
 
-// add a click event to all shapes
-layer.getChildren().forEach(function(node) {
-  node.on('click', function() {
-    // check if the transformer is on the same shape
-    if (transformer.nodes()[0] === this) {
-      // hide the transformer
-      transformer.hide();
-    } else {
-      // update the transformer with the new shape
-      transformer.nodes([this]);
-      // show the transformer
-      transformer.show();
-    }
-    // force the layer to draw
-    layer.draw();
+function addClickEventForTransformer() {
+  // remove all existing click events
+  layer.getChildren().forEach(function(node) {
+    node.off('click');
   });
-});
+  layer.off('click');
 
-// add a click event to the background
-layer.on('click', function(e) {
-  // check if the click was on a shape
-  if(e.target === layer){
-    //// hide the transformer when the background is clicked
-transformer.hide();
-// force the layer to draw
-layer.draw();
+  // add a click event to all shapes
+  layer.getChildren().forEach(function(node) {
+    node.on('click', function() {
+      // check if the transformer is on the same shape
+      if (transformer.nodes()[0] === this) {
+        // hide the transformer
+        transformer.hide();
+      } else {
+        // update the transformer with the new shape
+        transformer.nodes([this]);
+        // show the transformer
+        transformer.show();
+      }
+      // force the layer to draw
+      layer.draw();
+    });
+  });
+
+  // add a click event to the background
+  layer.on('click', function(e) {
+    // check if the click was on a shape
+    if(e.target === layer){
+      //// hide the transformer when the background is clicked
+      transformer.hide();
+      // force the layer to draw
+      layer.draw();
+    }
+  });
 }
-});
+
+addClickEventForTransformer()
+
+
+// IMAGE UPLOAD
+function handleFileSelect() {
+  var input = document.getElementById("fileInput");
+  var file = input.files[0];
+  var reader = new FileReader();
+
+  reader.onload = function(event) {
+    var imageObj = new Image();
+    imageObj.src = event.target.result;
+
+    imageObj.onload = function() {
+      var image = new Konva.Image({
+        x: 50,
+        y: 50,
+        image: imageObj,
+        width: 300,
+        height: 300
+      });
+
+      var group = new Konva.Group({
+        x: 50,
+        y: 50,
+        draggable: true
+      });
+
+      group.add(image);
+        layer.add(group);
+        layer.draw();
+      };
+    };
+    reader.readAsDataURL(file);
+    
+    addClickEventForTransformer()
+}
